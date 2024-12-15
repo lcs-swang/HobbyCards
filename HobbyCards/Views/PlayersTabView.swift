c48bba4c85a4d497767b76a86f06537493c4195a
Susie Wang <SWang@lcs.on.ca>
2024-12-14 22:16:35 -0500

    i get the colours and am trying to put it under the words but somehow I can't figure it out

--- HobbyCards/Views/PlayersTabView.swift
+++ HobbyCards/Views/PlayersTabView.swift
@@ -1,45 +1,45 @@
-//
-//  PlayersTabView.swift
-//  HobbyCards
-//
-//  Created by Susie Wang on 2024-12-14.
-//
 import SwiftUI
-
 
 struct HockeyCardView: View {
     let player: HockeyPlayer
     
     var body: some View {
-        VStack {
-            // Color blocks: Three different color sections
-            HStack(spacing: 0) {
-                Rectangle()
-                    .fill(Color.gray)  // First color block
-                    .frame(width: 100)
-                
-                Rectangle()
-                    .fill(Color.blue)  // Second color block
-                    .frame(width: 100)
-                
-                Rectangle()
-                    .fill(Color.purple)  // Third color block
-                    .frame(width: 100)
+        HStack {
+            // Color blocks: Three equally spaced color sections
+            
+                VStack(spacing:0) {
+                    Rectangle()
+                        .fill(Color.gray) // First color block
+                    
+                    Rectangle()
+                        .fill(Color.blue) // Second color block
+                    
+                    Rectangle()
+                        .fill(Color.purple) // Third color block
+                }
             }
-            .frame(height: 100)  // Height for the color blocks section
+            .frame(height: 1000) // Height for the color blocks section
+            
             VStack {
                 Text(player.name)
                     .font(.title)
                     .fontWeight(.bold)
-                ZStack {
+                Spacer()
+                Spacer()
+                VStack {
                     HockeyStick()
-                        .size(width: 60,height: 60)
+                        .size(width: 100, height: 150)
+                        .rotationEffect(.degrees(0))
                     HockeyStick()
-                        .size(width: 60,height: 60)
+                        .size(width: 100, height: 150)
+                        .rotationEffect(.degrees(180))
                 }
+                
                 Text(player.position)
                     .font(.headline)
                     .foregroundColor(.black)
+                
+                Spacer()
                 Spacer()
                 Text(player.team)
                     .font(.headline)
@@ -49,30 +49,28 @@ struct HockeyCardView: View {
             .frame(width: 250, height: 300)
             .background(Color.white)
             .cornerRadius(10)
-            
         }
     }
-    
-    // Main view with TabView to show all the players in individual tabs
-    struct ContentView: View {
-        var body: some View {
-            TabView {
-                ForEach(players) { player in
-                    HockeyCardView(player: player)
-                        .tabItem {
-                            Text(player.name)
-                            Image(systemName: "person.fill")
-                        }
-                }
+
+
+// Main view with TabView to show all the players in individual tabs
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            ForEach(players) { player in
+                HockeyCardView(player: player)
+                    .tabItem {
+                        Text(player.name)
+                        Image(systemName: "person.fill")
+                    }
             }
-            .tabViewStyle(PageTabViewStyle())
         }
+        .tabViewStyle(PageTabViewStyle())
     }
-    
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
