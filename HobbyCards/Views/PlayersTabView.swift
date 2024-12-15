7a583c878760e190e25032ae4cb940e131d9046d
Susie Wang <SWang@lcs.on.ca>
2024-12-14 19:34:29 -0500

    kind of got the different colors but not quite there yet

--- HobbyCards/Views/PlayersTabView.swift
+++ HobbyCards/Views/PlayersTabView.swift
@@ -12,50 +12,67 @@ struct HockeyCardView: View {
     
     var body: some View {
         VStack {
-            Text(player.name)
-                .font(.title)
-                .fontWeight(.bold)
-            ZStack {
-                HockeyStick()
-                    .size(width: 60,height: 60)
-                HockeyStick()
-                    .size(width: 60,height: 60)
+            // Color blocks: Three different color sections
+            HStack(spacing: 0) {
+                Rectangle()
+                    .fill(Color.gray)  // First color block
+                    .frame(width: 100)
+                
+                Rectangle()
+                    .fill(Color.blue)  // Second color block
+                    .frame(width: 100)
+                
+                Rectangle()
+                    .fill(Color.purple)  // Third color block
+                    .frame(width: 100)
             }
-            Text(player.position)
-                .font(.subheadline)
-                .foregroundColor(.gray)
-            Spacer()
-            Text(player.team)
-                .font(.subheadline)
-                .foregroundColor(.blue)
-        }
-        .padding()
-        .frame(width: 250, height: 300)
-        .background(Color.white)
-        .cornerRadius(10)
-
-    }
-}
-
-// Main view with TabView to show all the players in individual tabs
-struct ContentView: View {
-    var body: some View {
-        TabView {
-            ForEach(players) { player in
-                HockeyCardView(player: player)
-                    .tabItem {
-                        Text(player.name)
-                        Image(systemName: "person.fill")
-                    }
+            .frame(height: 100)  // Height for the color blocks section
+            VStack {
+                Text(player.name)
+                    .font(.title)
+                    .fontWeight(.bold)
+                ZStack {
+                    HockeyStick()
+                        .size(width: 60,height: 60)
+                    HockeyStick()
+                        .size(width: 60,height: 60)
+                }
+                Text(player.position)
+                    .font(.headline)
+                    .foregroundColor(.black)
+                Spacer()
+                Text(player.team)
+                    .font(.headline)
+                    .foregroundColor(.blue)
             }
+            .padding()
+            .frame(width: 250, height: 300)
+            .background(Color.white)
+            .cornerRadius(10)
+            
         }
-        .tabViewStyle(PageTabViewStyle())
     }
-}
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    // Main view with TabView to show all the players in individual tabs
+    struct ContentView: View {
+        var body: some View {
+            TabView {
+                ForEach(players) { player in
+                    HockeyCardView(player: player)
+                        .tabItem {
+                            Text(player.name)
+                            Image(systemName: "person.fill")
+                        }
+                }
+            }
+            .tabViewStyle(PageTabViewStyle())
+        }
+    }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
